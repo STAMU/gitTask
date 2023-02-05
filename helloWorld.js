cf611c5ddd5b156bf3e0693472deb55d86f8d435
Supen TAMU <supen.tamu@gmail.com>
2023-02-05 11:49:42 +0000

    user input displayed in helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,6 @@
 // This is for the compulsory task 1 of Main TASK 43
+// User asked to input a data and it is displayed
 
-// Displays "Hello World!"
+let message= prompt("Please type your message:");
 
-console.log(" Git is Awesome! ");
+console.log( message );
